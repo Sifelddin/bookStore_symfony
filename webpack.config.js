72d27df1323c9ff92ef6e92c4906b18059762857
ali tarek <bekhouche3939@gmail.com>
2022-05-16 16:51:30 +0200

    tailwind first configurations

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('test', './assets/test.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -70,6 +71,12 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    .enablePostCssLoader((options) => {
+        options.postcssOptions = {
+            config: './postcss.config.js'
+        }
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
