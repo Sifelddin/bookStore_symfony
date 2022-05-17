5206b523db82f17a6d73f0ddda173f1548173df9
ali tarek <bekhouche3939@gmail.com>
2022-05-17 09:31:39 +0200

    tailwindcss final config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
-let tailwindcss = require('tailwindcss');
+let tailwindcss = require("tailwindcss")
+
 module.exports = {
   plugins: [
     tailwindcss('./tailwind.config.js'),
     require('postcss-import'),
-    require('autoprefixer'),
-  ],
-};
+    require('autoprefixer')
+  ]
+}
