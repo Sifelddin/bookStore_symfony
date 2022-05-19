cb9aa863edc0ba47a98840f89bdc34ad2af8f889
ali tarek <bekhouche3939@gmail.com>
2022-05-19 16:41:40 +0200

    admin section witn sidebar

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/category')]
+#[Route('/categories')]
 class CategoryController extends AbstractController
 {
     #[Route('/', name: 'app_category_index', methods: ['GET'])]
